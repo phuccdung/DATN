1e5fe6839c9fae1f89ae1df9319d108a2bd0a099
phuccdung <82568398+phuccdung@users.noreply.github.com>
2023-01-01 23:14:05 +0700

    [update] Origins

--- api_main/config/allowedOrigins.js
+++ api_main/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://www.multi-mart.com',
-    'http://127.0.0.1:5500',
+    'http://156.67.219.180:3500',
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:3006'
