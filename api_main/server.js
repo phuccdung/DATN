bc3684065e0b68157c6ccf3af1375cfd9dd251bc
phuccdung <82568398+phuccdung@users.noreply.github.com>
2023-01-01 23:28:34 +0700

    [update] cors

--- api_main/server.js
+++ api_main/server.js
@@ -24,7 +24,8 @@ app.use(logger);
 app.use(credentials);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app(cors());
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
