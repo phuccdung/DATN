dbe47732d730aebe8855f8f3b90dda72814c574f
phuccdung <82568398+phuccdung@users.noreply.github.com>
2023-01-05 13:42:58 +0700

    [update] cors

--- api_main/config/allowedOrigins.js
+++ api_main/config/allowedOrigins.js
@@ -5,6 +5,9 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:3006',
+    'http://156.67.219.180:3500',
+    'http://156.67.219.180:3000',
+    'http://156.67.219.180:3006',
 ];
 
 module.exports = allowedOrigins;
